cf6a9913ba3ada736099f2565638c76df11c9248
4444dogs <85034387+4444dogs@users.noreply.github.com>
2022-05-10 13:04:13 -0600

    Add Current Progress from Local

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+INFO: This is a early prototype of my iPBL game.
+Players will write in the function below to put together three strings that have coding facts.
+
+NOTES: Remove the answer from the function before the big day.
+"""
+
+import test
+
+# Function that players write in
+def unscramble(str1, str2, str3):
+    # ANSWER KEY BELOW
+    return str1 + str2 + str3
+
+# Facts
+print(unscramble(test.fact1_1, test.fact1_2, test.fact1_3))
---
+++ test.py
@@ -0,0 +1,8 @@
+# Script for holding variables in main.py (DO NOT SHOW THIS TO PLAYERS)
+
+# FACTS
+
+# Fact 1 (THIS IS A TEST! REMOVE IT FROM THE FINAL PRODUCT!)
+fact1_1 = "Coding "
+fact1_2 = "is "
+fact1_3 = "fun!"
