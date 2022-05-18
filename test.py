67dfd954697427aceb538f981e53fe4e6c9b8698
4444dogs <85034387+4444dogs@users.noreply.github.com>
2022-05-18 11:56:11 -0600

    Add Advertisement to Congrats

--- test.py
+++ test.py
@@ -18,6 +18,6 @@ fact3_2 = " improve your "
 fact3_3 = "creativity!"
 
 #Congrats Message
-congrats_1 = "Congrats! You have completed this coding challenge!"
+congrats_1 = "Congrats! You have completed this coding challenge! Maybe you could try out the cubing station next."
 congrats_2 = ""
 congrats_3 = ""
